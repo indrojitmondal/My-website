1093b953bce2d001d584c37666bffc47b4c2b470
indroji-mondal <indro.cse.bu@gmail.com>
2025-05-06 21:00:49 +0600

    Added Hire me

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,18 @@ import Banner from './Banner';
 import TechnologiesIWorkWith from './TechnologiesIworkWith';
 import WhatIWorkOn from './WhatIWorkOn';
 import MyRecentProjects from './MyRecentProjects';
+import HireMe from './HireMe';
+
 
 const Home = () => {
     return (
-        <div className="min-h-screen" >
+        <div >
               <Banner></Banner>
               <TechnologiesIWorkWith></TechnologiesIWorkWith>
               <WhatIWorkOn></WhatIWorkOn>
               <MyRecentProjects></MyRecentProjects>
+              <HireMe></HireMe>
+              
         </div>
     );
 };
